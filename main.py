5e9b60ba1553d6c549bdace37989ec3693289101
JT - LC101 <joetopp@gmail.com>
2017-10-04 22:54:14 -0500

    setting up form and adding welcome page

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    return render_template('welcome.html')
+
 @app.route("/")
 def index():
     return render_template('base.html')
